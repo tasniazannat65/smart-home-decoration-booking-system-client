9ba74ba52aed033209af3eaab0337c239c480dc2
tasniazannat65 <tasniazannat9@gmail.com>
2026-02-15 23:14:00 +0600

    modify navbar

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,35 +1,32 @@
-import React from 'react';
-import Navbar from '../Components/Shared/Navbar/Navbar';
-import { Outlet, useNavigation } from 'react-router';
-import Footer from '../Components/Shared/Footer/Footer';
-import Loading from '../Components/Shared/Loading/Loading';
-import ScrollTop from '../Components/Shared/Scrolltop/ScrollTop';
+import React from "react";
+import Navbar from "../Components/Shared/Navbar/Navbar";
+import { Outlet, useNavigation } from "react-router";
+import Footer from "../Components/Shared/Footer/Footer";
+import Loading from "../Components/Shared/Loading/Loading";
+import ScrollTop from "../Components/Shared/Scrolltop/ScrollTop";
 
 const MainLayout = () => {
-    const navigation = useNavigation();
-    return (
-        <div>
-            <header>
-                <nav>
-                    <Navbar/>
-                </nav>
-            </header>
-            <main className='min-h-screen pt-16 md:pt-20'>
-                {
-                    navigation.state === 'loading' && (<Loading/>)
-                }
-                <Outlet/>
+  const navigation = useNavigation();
+  return (
+    <div>
+      <header>
+        <nav>
+          <Navbar />
+        </nav>
+      </header>
+      <main className="min-h-screen pt-[70px] md:pt-20">
+        {navigation.state === "loading" && <Loading />}
+        <Outlet />
 
-                 <section>
-                <ScrollTop/>
-            </section>
-            </main>
-            <footer>
-                <Footer/>
-            </footer>
-            
-        </div>
-    );
+        <section>
+          <ScrollTop />
+        </section>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default MainLayout;
